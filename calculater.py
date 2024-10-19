5dd54be9fbde4ca2a3da0d14b9a78736c8cf6d0c
Sreyan <sreyanmithra@gmail.com>
2024-10-19 17:04:09 +0530

    Update calculater.py
    Exception handling : Not to get a error when person uses string instead of integer as a input, instead prints to type only integers.

--- calculater.py
+++ calculater.py
@@ -1,23 +1,32 @@
-if __name__=="__main__":
-#def cal():
-    
-    while True:
-        a=int(input("enter your first number :"))
-        c=input("enter your sign {+,-,*,/}:")
-        b=int(input("enter your second number:"))
-        print(a,c,b)
-        if c == '+':
-            print(a+b)
-        elif c == '-':
-            print(a-b)
-        elif c == '*':
-            print(a*b)
-        elif c == '/':
-            print(a/b)
-        else:
-            print("               !!!!!!!! .please choose correct sign. !!!!!!!!              ")
-        d=input(" if you want to close to type (off)\n")
-        if d== 'off':
-            break
-        print("calculater is again started")
-print("calculater is closed")
+try:  # Telling the code to not to return error if input of those are strings.
+
+    if __name__ == "__main__":
+        # def cal():
+
+        while True:
+
+            a = int(input("enter your first number : "))
+            c = input("\nenter your sign {+,-,*,/}: ")
+            b = int(input("\nenter your second number: "))
+
+            print(f"\n{a} {c} {b}")
+            if c == '+':
+                print(a + b)
+            elif c == '-':
+                print(a - b)
+            elif c == '*':
+                print(a * b)
+            elif c == '/':
+                print(a / b)
+            else:
+                print("               !!!!!!!! .please choose correct sign. !!!!!!!!              ")
+
+            d = input("\nif you want to close to type (off): ")
+            if d == 'off':
+                break
+
+    print("calculater is closed")
+
+except:             # If the user entered string instead of a integer print the below statement instead of throwing a error.
+
+    print("Enter only integers not strings")
